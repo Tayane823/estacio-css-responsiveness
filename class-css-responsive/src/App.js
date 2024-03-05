69be50d851577051a28958c048f936f53a53f6fd
Figueiredo <tayanealves823@gmail.com>
2024-03-05 19:24:14 -0300

    css-responsive

---
+++ class-css-responsive/src/App.js
@@ -0,0 +1,12 @@
+import ResponsiveClass from './Class/responsive-class/ResponsiveClass';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveClass />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ class-css-responsive/src/Class/responsive-class/ResponsiveClass.js
@@ -0,0 +1,47 @@
+import  React from 'react'
+import './ResponsiveClass.css' 
+
+const ResponsiveClass = () => {
+    return (
+        <div className='content'>
+            <h1>Css e designer responsivo - Técnicas e boas práticas de responsividade</h1>
+
+            <aside>
+                <h2>Principais conceitos e métodos:</h2>
+                <ul>
+                    <li>Flexbox</li>
+                    <li>Media Queries</li>
+                    <li>Grid Layout</li>
+                    <li>Layout fluído</li>
+                    <li>Viewport meta tag</li>
+                </ul>
+
+            </aside>
+
+            <main>
+               <h3>O que você precisa saber sobre como criar uma página com css responsivo</h3> 
+
+                <p>
+                    lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet
+                    lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem 
+                    ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet
+                    lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem 
+                    ipsum dolor set ametlorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet
+                    lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem 
+                    ipsum dolor set ametlorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet
+                    lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem 
+                    ipsum dolor set ametlorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet
+                    lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem ipsum dolor set amet lorem 
+                    ipsum dolor set amet
+                </p>
+            </main>
+            
+
+            <footer>
+                <p> Tayane Cristina, março de 2024 &copy; Todos os direitos reservados.</p>
+            </footer>
+        </div>
+    ) 
+
+}
+export default ResponsiveClass;
